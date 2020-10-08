75718a2d182c004b81413e0c4ed7a5486d713955
Aarya V <72181916+aarya-xvi@users.noreply.github.com>
2020-10-08 12:23:18 +0530

    Update error_log.py

--- error_log.py
+++ error_log.py
@@ -1,23 +1,31 @@
 import os
 import re
 
+# path of the directory
 mypath = "D:\Scripts\Concepts"
 
 for root, dirs, files in os.walk(mypath):
-    error_lines = set()  # holds lines
+    error_lines = set()  # holds lines with errors
 
-    for file in files:
-        line_num = 0
-        with open(os.path.join(root, file), "r") as auto:
-            a = auto.read(50000)
-            for line in a.splitlines():
+    for file in files:   # for each file
+        line_num = 0     # for counting lines of each file
+        with open(os.path.join(root, file), "r") as auto:  # open file
+            a = auto.read(50000)  # assigning the content of the file into variable a
+            for line in a.splitlines():  # splitting the content of each line
                 line_num += 1
-                Errors = re.findall('(?i)error', line)
+                Errors = re.findall('(?i)error', line)  # makes case insensitive
                 for error in Errors:
-                    if error in line and line not in error_lines:
-                       # print(file, line_num, line)
+                    if error in line:
                         error_lines.add(line)
-                        print(file, line_num, line)
+                        # print(file, line_num, line)
+# print('\n'.join(error_lines))
+
+write = '\n'.join(error_lines)
+
+with open('D:\Scripts\error_python.txt', 'a') as output:
+    output.write(str(write))
+
+
 
 
 
